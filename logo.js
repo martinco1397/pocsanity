628f9d050d48967e0ab2f4e818c79699405dee2c
Chanderpaul Martin <chanderpaul.martin@gmail.com>
2020-12-14 11:07:51 -0500

    Changes

---
+++ logo.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import styles from "./logo.css";
+
+
+
+const logo = () => {
+    return (
+        <img className={styles.logo} src="/static/logo.jpg" alt=""/>
+    )
+}
+
+export default logo
